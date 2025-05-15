5032601360045a63c5dea2aef352cdb23c39402c
jon56762 <successjonathan567@gmail.com>
2025-05-15 22:18:29 +0100

    Done with the solving of xvalues, xsquare and y assignment

--- forloop.php
+++ forloop.php
@@ -1,57 +1,18 @@
 <?php
-
-/*for ($i=0; $i <= 10; $i++) { 
-    echo "$i \n";
-}*/
-
-/*for ($i=-5; $i <= 23; $i++) { 
-    echo "$i \n";
-}
-
-echo "---------------\n";
-
-for ($i=3.5; $i >= -5; $i--) { 
-    echo "$i \n";
-}*/
-
-echo "(1)List all odd numbers between 3 - 65\n";
-for ($i= 3; $i <= 65; $i++) { 
-    
-    if ($i % 2 == 0) {
-        echo "";
+for ($question1=3; $question1 <= 65 ; $question1++) { 
+    // Generate the odd numbers
+    if ($question1 % 2 == 0) {
+        echo"";
     } else {
-        echo "$i \n";
+        echo $question1;
+        echo ";";
+    }
+
+    // Generate the intercept numbers
+    for ($intercept=15; $intercept <= 100 ; $intercept++) { 
+        if ($question1 == $intercept) {
+            echo $intercept;
+            echo ",";
+        }
     }
 }
-
-$x1 = 15;
-$x2 = 100;
-$y1 = 51;
-$y2 = 70;
-
-echo "(2)Find the intercept between 15 and 100\n";
-for ($i=$y1; $i <= $y2 ; $i++) { 
-    echo "$i \n";
-}
-
-echo "(3)Find the union between 51 and 70\n"; 
-for ($i=$x1; $i <= $x2 ; $i++) { 
-    echo "$i \n";
-}
-
-
-// echo "(2)Find the intercept between 15 and 100\n";
-// for ($i=15; $i <= 100; $i++) { 
-//     if ($i %  15== 0) {
-//         echo "";
-//     }else {
-//         echo "$i \n";
-//     }
-// }
-
-// echo "(3)Find the union between 51 and 70\n";
-// for ($i=51; $i <= 70; $i++) { 
-//     if ($i == 51 % 70) {
-//         echo "$i \n";
-//     }
-// }
---
+++ graph.php
@@ -0,0 +1,34 @@
+<?php
+
+$xvalues = [-3, -2, -1, 0, 1, 2, 3];
+$xsquare = [];
+$y = [];
+
+// Calculating xsquare and y 
+for ($solve=0; $solve < 7; $solve++) { 
+    $x = $xvalues[$solve];
+    $xsquare[$solve] = $x * $x;
+    $y[$solve] = $xsquare[$solve] + $x - 2;
+}
+
+// applying the calucated xsquare to the array
+echo "x :";
+for ($solve=0; $solve < 7; $solve++) { 
+    echo $xvalues[$solve]. "|";
+}
+
+echo "\n";
+
+// applying the calucated y to the array
+
+echo "xsquare :";
+for ($solve=0; $solve < 7; $solve++) { 
+    echo $xsquare[$solve] . "|";
+}
+
+echo "\n";
+
+echo "y :";
+for ($solve=0; $solve < 7; $solve++) { 
+    echo $y[$solve]. "|";
+}
