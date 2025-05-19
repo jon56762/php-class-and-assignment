f657af4296ecea7f208005df93378c4872dfac69
jon56762 <successjonathan567@gmail.com>
2025-05-19 19:44:10 +0100

    Restructure the graph table with while loop

--- graph.php
+++ graph.php
@@ -1,23 +1,39 @@
 <?php
-
-$x = readline("Input the first number coeffient:");
-$xvalues = readline("Input the second coeffient:");
-
-$solve = $x;
-while ($solve <= $xvalues) { 
-// Calculating xsquare and y
-    $xsquare = pow($solve, 2);
-    $y = $xsquare + $solve - 2;
+echo "y = xsquare + x -2 from:\n";
+$x_start = readline("x = (x-start)");
+$x_end = readline("to x = (x-end)");
 
 // applying the calucated xsquare to the array
-
-echo "xsquare = ";
-echo "$xsquare ";
-
-// applying the calucated y to the array
-
-echo "y = "; 
-echo "$y \n";
-
-$solve++;
+echo "x |";
+$x = $x_start;
+while ($x <= $x_end) {
+    echo "$x |";
+    $x++;
 }
+echo "\n";
+
+echo "xsquare |";
+$x = $x_start;
+while ($x <= $x_end) {
+    $xsquare = pow($x, 2);
+    echo "$xsquare |";
+    $x++;
+}
+echo "\n";
+
+echo "-2 |";
+$x = $x_start;
+while ($x <= $x_end) {
+    echo "-2 |";
+    $x++;
+}
+echo "\n";
+
+echo "y |";
+$x = $x_start;
+while ($x <= $x_end) {
+    $y = $xsquare + $x - 2;
+    echo "$x |";
+    $x++;
+}
+echo "\n";
