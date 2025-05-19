7986d5236728cd64599e211b7d519b62870e108d
jon56762 <successjonathan567@gmail.com>
2025-05-19 15:05:48 +0100

    Done changing forloop to whileloop

--- graph.php
+++ graph.php
@@ -1,45 +1,23 @@
 <?php
 
-$xvalues = [-3, -2, -1, 0, 1, 2, 3,];
-$xsquare = [];
-$two = [];
-$y = [];
+$x = readline("Input the first number coeffient:");
+$xvalues = readline("Input the second coeffient:");
 
-// Calculating xsquare and y 
-for ($solve=0; $solve < 7; $solve++) { 
-    $x = $xvalues[$solve];
-    $xsquare[$solve] = $x * $x;
-    $two[$solve] = - 2; 
-    $y[$solve] = $xsquare[$solve] + $x - 2;
-}
-
-// applying the calucated xvalues to the array
-echo "x :";
-for ($solve=0; $solve < 7; $solve++) { 
-    echo "{$xvalues[$solve]} |";
-}
-
-echo "\n";
+$solve = $x;
+while ($solve <= $xvalues) { 
+// Calculating xsquare and y
+    $xsquare = pow($solve, 2);
+    $y = $xsquare + $solve - 2;
 
 // applying the calucated xsquare to the array
 
-echo "xsquare :";
-for ($solve=0; $solve < 7; $solve++) { 
-    echo "{$xsquare[$solve]} |";
-}
-
-echo "\n";
-
-echo "two :";
-for ($solve=0; $solve < 7; $solve++) { 
-    echo "{$two[$solve]} |";
-}
-
-echo "\n";
+echo "xsquare = ";
+echo "$xsquare ";
 
 // applying the calucated y to the array
 
-echo "y :";
-for ($solve=0; $solve < 7; $solve++) { 
-    echo "{$y[$solve]} |";
+echo "y = "; 
+echo "$y \n";
+
+$solve++;
 }
